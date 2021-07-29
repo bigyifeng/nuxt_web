4571635e84985e4a525fbc46e110ff2151445bf9
yifeng <478297605@qq.com>
2021-07-29 17:36:04 +0800

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+# github_blog
+
+## Build Setup
+
+```bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# generate static project
+$ yarn generate
+```
+## Nuxt
+此分支是使用 Nuxt 静态部署生成的个人网站
+
+## 为什么使用Nuxt？
+1. 简便：开发可以直接用 Vue 语法，不用使用其他东西
+2. SEO：可以在使用 Vue 的基础上做SEO
+3. 学以致用：学习了一门技术如果不放在实战中，那将毫无意义
---
+++ pages/index.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="main">
+    <div class="bg">
+      <h1 class="hide">洪一锋</h1>
+      <div class="content w">
+        <div class="logo">
+          <img
+            src="https://assets-service.obs.cn-south-1.myhuaweicloud.com/production/mp_c8d760a0-8307-11eb-a3eb-d500e74411b7/5d531e1b-4681-4b53-85f9-7883b6d3c168.png"
+            alt="">
+        </div>
+        <ul class="nav">
+          <li v-for="item in navList" :key="item.href">
+            <a class="menu-text" :href="item.herf">{{item.text}}</a>
+          </li>
+        </ul>
+        <p>时光正好，未来可期，加油 ！
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      navList: [
+        {
+          text: '知识库',
+          herf: './repository/index.html'
+        },
+        {
+          text: '学习打卡',
+          herf: './src/views/study/study.html'
+        },
+        {
+          text: '演示项目',
+          herf: './src/views/items/items.html'
+        },
+        {
+          text: 'Github',
+          herf: 'https://github.com/bigyifeng/bigyifeng.github.io'
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+.main {
+  width: 100%;
+  height: 100%;
+  min-height: 700px;
+  background: url('https://assets-service.obs.cn-south-1.myhuaweicloud.com/production/mp_c8d760a0-8307-11eb-a3eb-d500e74411b7/31d0b252-932b-4ea2-8531-9b1ff117436a.jpeg')
+    no-repeat;
+  background-size: cover;
+}
+.bg {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: grammaticalization 2s linear infinite alternate;
+}
+
+h1 {
+  display: none;
+}
+
+.content {
+  width: 700px;
+  height: auto;
+  padding: 50px 0;
+  transform: translateY(-80px);
+}
+
+.logo {
+  animation: smallToBig 2s linear infinite alternate;
+  margin: 0 auto;
+  width: 170px;
+  height: 170px;
+  margin-bottom: 20px;
+}
+
+.logo img {
+  width: 100%;
+  height: 100%;
+}
+
+.nav {
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  background: rgba(255, 255, 255, 0.7);
+  padding: 20px;
+  border-radius: 30px;
+}
+
+.nav li {
+  color: #409eff;
+  min-width: 80px;
+  font-size: 20px;
+}
+
+.nav li a {
+  color: #409eff;
+  font-size: 20px;
+}
+p {
+  color: #43b98e;
+  font-size: 20px;
+  text-align: center;
+  margin-top: 20px;
+}
+
+/* 背景虚化 */
+@keyframes grammaticalization {
+  0% {
+    backdrop-filter: blur(1px);
+  }
+
+  50% {
+    backdrop-filter: blur(6px);
+  }
+
+  100% {
+    backdrop-filter: blur(1px);
+  }
+}
+
+@keyframes smallToBig {
+  0% {
+    transform: scale(1);
+  }
+
+  50% {
+    transform: scale(1.2);
+  }
+
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
